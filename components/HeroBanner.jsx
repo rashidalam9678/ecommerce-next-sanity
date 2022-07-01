efcde8cd87cb3908346cdb94f14adad7765536c7
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-07-01 11:49:36 +0530

    hero section done

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+        <div>
+            <p className="beats-solo">
+                SMALL TEXT
+            </p>
+            <h3>
+                MID TEXT
+            </h3>
+            <img className="hero-banner-image" src="" alt="headphones" />
+        </div>
+        <Link href="/product/ID">
+            <button type="button">
+                BUTTON TEXT
+            </button>
+        </Link>
+        <div className="desc">
+            <h5>Description</h5>
+            <p>Description</p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default HeroBanner
---
+++ sanity_ecommerce/schemas/schema.js
@@ -0,0 +1,13 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import banner from './banner'
+import product from './product'
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    banner,
+    product,
+  ]),
+})
